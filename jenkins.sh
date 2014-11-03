a7b57dbfd749d018e54f750771669dd9bfacced6
Adrinems <adrianasorianom@gmail.com>
2014-11-03 09:10:53 -0600

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+pip install -r requirements.txt
+autopep8 -ir .
+flake8 --max-complexity=3 --exclude=*.txt,*.md --max-line-length=200 .
+
+cd UnitTest
+python TestTennis.py -v
+coverage run --branch TestTennis.py
+coverage report -m
+coverage html --title="Coverage about Tennis"
